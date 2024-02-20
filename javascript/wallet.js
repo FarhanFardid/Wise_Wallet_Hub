9772761abc5147444bc9b733b64ef4205efe2267
FarhanFardid <farhanfardid009@gmail.com>
2024-02-20 20:02:14 +0600

    wallet.js file created

---
+++ javascript/wallet.js
@@ -0,0 +1,11 @@
+//  Javascript makes the webpage functional and Interactive
+
+// console.log("Hello from JS file");
+
+// Calculate Expense Button
+
+const expenseCalculateBtn = document.getElementById("calculate-btn");
+ expenseCalculateBtn.addEventListener('click', function(){
+
+    // console.log("Calculate Button clicked" );
+ })
